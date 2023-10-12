95beb42f406c3000f990caaf56537fd57d66243e
Svobodennn <melih.sarac@hotmail.com>
2023-10-12 13:20:36 +0300

    Abstract Usage

---
+++ 06-OOP/06-abstract.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * Usage of Abstract (Abstraction)
+Abstract classes contain abstract methods within them.
+Classes derived from an abstract class must have the
+abstract methods defined in the abstract class.
+It is like a schema
+
+ *Features
+
+Can include abstract methods and constants.
+Can include regular methods, properties, and constants.
+Can be defined as public, protected, or private.
+A single class can only be derived from one abstract
+class.
+*/
+
+Namespace Abstractt;
+
+abstract class Product{
+    public $pName;
+    public $pPrice;
+    public $pTax;
+
+    abstract public function productName($pName);
+    abstract public function productPrice($pPrice);
+    abstract public function productTax($pTax);
+
+    public function addProduct(){
+        //SAVE TO DB
+        echo "Product name: ".$this->pName."<br>";
+        echo "Product price: ".$this->pPrice."<br>";
+        echo "Product tax: ".$this->pTax."<br>";
+    }
+}
+
+class Drink extends Product { // generate>implement methods
+
+    public function productName($pName)
+    {
+        // TODO: Implement productName() method.
+        $this->pName = $pName;
+    }
+
+    public function productPrice($pPrice)
+    {
+        // TODO: Implement productPrice() method.
+        $this->pPrice = $pPrice;
+    }
+
+    public function productTax($pTax)
+    {
+        // TODO: Implement productTax() method.
+        $this->pTax = $pTax;
+    }
+}
+class Textile extends Product{
+    public function productName($pName)
+    {
+        // TODO: Implement productName() method.
+        $this->pName = $pName;
+    }
+
+    public function productPrice($pPrice)
+    {
+        // TODO: Implement productPrice() method.
+        $this->pPrice = $pPrice;
+    }
+
+    public function productTax($pTax)
+    {
+        // TODO: Implement productTax() method.
+        $this->pTax = $pTax;
+    }
+
+}
+
+$Drink = new Drink();
+
+$Drink->productName('Cola');
+$Drink->productPrice('30TL');
+$Drink->productTax('8');
+
+$Drink->addProduct();
+
+$Textile = new Textile();
+$Textile->productName('Pants');
+$Textile->productPrice('430TL');
+$Textile->productTax('18');
+
+$Textile->addProduct();
