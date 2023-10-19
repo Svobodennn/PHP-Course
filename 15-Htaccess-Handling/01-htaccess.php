24a912ea878373fa956f0b56a738d23ffac8e422
Svobodennn <melih.sarac@hotmail.com>
2023-10-19 16:46:19 +0300

    Htaccess Handling

---
+++ 15-Htaccess-Handling/01-htaccess.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Folder Access Control
+Authorization and Authentication
+URL Rewriting
+Blocking Access to Files and Directories
+Directory Listing
+Custom Error Messages
+Setting MIME/File Types
+Cache Control
+*/
+
+echo "This is index";
